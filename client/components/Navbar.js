742ba643344d97a9713e8cd79a1c463963f37e3e
Andrey Statkevskiy <andstatik@gmail.com>
2022-10-18 10:24:52 +0900

    cosmetics: sidebar and social media bar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -4,20 +4,28 @@ import {Link} from 'react-router-dom'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>My Website</h1>
+    {/* The navbar will show these links after you log in */}
     <nav>
       {(
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/">Home</Link>
-          <Link to="/projects">Projects</Link>
-          <Link to="/resume">Resume</Link>
-          <Link to="/about">About Me</Link>
-          <Link to="/contact">Contact</Link>
+        <div class="wrapper">
+          <div class="sidebar">
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About Me</Link></li>
+              <li><Link to="/projects">Projects</Link></li>
+              <li><Link to="/resume">Resume</Link></li>
+              <li><Link to="/blog">Blog</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
+            </ul>
+            <div class="social_media">
+              <a href="https://www.linkedin.com/in/andrey-statkevskiy/" target='_blank'><i class="fab fa-linkedin"></i></a>
+              <a href="https://github.com/AndStatik" target='_blank'><i class="fab fa-github"></i></a>
+              <a href="https://www.instagram.com/andstatik/" target='_blank'><i class="fab fa-instagram"></i></a>
+            </div>
+          </div>
         </div>
       )}
     </nav>
-    <hr />
   </div>
 )
 
