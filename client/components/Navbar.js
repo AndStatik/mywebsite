50b768beb213d2d491958546a188e54c95c3b5eb
Andrey Statkevskiy <andstatik@gmail.com>
2022-10-21 09:01:49 +0900

    Cosmetics: First-level dropdown menu contains icons

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -3,6 +3,12 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import MenuIcon from '../../public/MenuIcon.svg'
 import ArrRight from '../../public/ArrRight.svg'
+import SettLeft from '../../public/SettLeft.svg'
+import IdLeft from '../../public/IdLeft.svg'
+import PortLeft from '../../public/PortLeft.svg'
+import ResLeft from '../../public/ResLeft.svg'
+import BlogLeft from '../../public/BlogLeft.svg'
+import EmailLeft from '../../public/EmailLeft.svg'
 
 
 const Navbar = () => {
@@ -16,8 +22,30 @@ const Navbar = () => {
           </a>
           {open && <div className='dropdown'>
               <a href='#' className='menu-item'>
-                <span className='icon-button'>LIcon</span>
-                Option
+                <span className='icon-button'><IdLeft /></span>
+                About Me
+                <span className='icon-right'><ArrRight /></span>
+              </a>
+              <a href='#' className='menu-item'>
+                <span className='icon-button'><PortLeft /></span>
+                Projects
+                <span className='icon-right'><ArrRight /></span>
+              </a>
+              <a href='#' className='menu-item'>
+                <span className='icon-button'><ResLeft /></span>
+                Resume
+              </a>
+              <a href='#' className='menu-item'>
+                <span className='icon-button'><BlogLeft /></span>
+                Blog
+              </a>
+              <a href='#' className='menu-item'>
+                <span className='icon-button'><EmailLeft /></span>
+                Contact Me
+              </a>
+              <a href='#' className='menu-item'>
+                <span className='icon-button'><SettLeft /></span>
+                Settings
                 <span className='icon-right'><ArrRight /></span>
               </a>
             </div>}
