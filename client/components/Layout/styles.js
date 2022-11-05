9b3f6ecd6661051419d4f495c12e06a25156a649
Andrey Statkevskiy <andstatik@gmail.com>
2022-11-05 19:50:00 +0900

    Cosmetics: Logo, complete css front for search, and a divider

--- client/components/Layout/styles.js
+++ client/components/Layout/styles.js
@@ -7,5 +7,8 @@ export const SLayout = styled.div`
 `;
 
 export const SMain = styled.main`
-  padding: calc(${v.xxlSpacing} * 2);
+  padding: calc(${v.smSpacing} * 2);
+  h1 {
+    font-size: 14px;
+  }
 `;
