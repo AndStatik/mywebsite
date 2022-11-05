41e5d0f283e115ab97c34b2e105ef90d92b51fb7
Andrey Statkevskiy <andstatik@gmail.com>
2022-11-05 11:05:52 +0900

    Consmetics: Styled Components Setup

--- client/App.js
+++ client/App.js
@@ -1,14 +1,36 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Routes from './Routes'
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./styles/theme";
+import Navbar from "./components/Navbar";
+import Routes from "./Routes";
+import { GlobalStyle } from "./styles/globalStyles";
+
+export const ThemeContext = React.createContext(null);
 
 const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Routes />
-    </div>
-  )
-}
+  const [theme, setTheme] = useState("light");
+  const themeStyle = theme === "light" ? lightTheme : darkTheme;
 
-export default App
+  return (
+    <ThemeContext.Provider value={{ setTheme, theme }}>
+       
+      <ThemeProvider theme={themeStyle}>
+        <GlobalStyle />
+        <Helmet>
+          <title>Andrey</title>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+        </Helmet>
+        <Navbar />
+        <Routes />
+      </ThemeProvider>
+    </ThemeContext>
+
+  );
+};
+
+export default App;
--- client/index.js
+++ client/index.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client';
-import {Provider} from 'react-redux'
-import {Router} from 'react-router-dom'
-import history from './history'
-import store from './store'
+// import {Provider} from 'react-redux'
+// import {Router} from 'react-router-dom'
+// import history from './history'
+// import store from './store'
 import App from './App'
 
 const container = document.getElementById('app');
 const root = createRoot(container);
 root.render(
-  <Provider store={store}>
-    <Router history={history}>
+  // <Provider store={store}>
+  //   <Router history={history}>
       <App />
-    </Router>
-  </Provider>
+  //   </Router>
+  // </Provider>
 )
