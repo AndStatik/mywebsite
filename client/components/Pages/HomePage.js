73bd7692efc3cd3489e848967b22bd810c6b5999
Andrey Statkevskiy <andstatik@gmail.com>
2022-11-16 08:42:30 +0900

    Cosmetics: Hi and description are relatively positioned and minimally styled

--- client/components/Pages/HomePage.js
+++ client/components/Pages/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import { Greet, MyName, Welcome } from './homeStyles'
+import { Greet, Greet1, MyName, Welcome } from './homeStyles'
 
 /**
  * COMPONENT
@@ -8,13 +8,13 @@ import { Greet, MyName, Welcome } from './homeStyles'
 export const HomePage = props => {
 
   return (
-    <div>
+    <Welcome>
       <MyName>ANDREY STATKEVSKIY</MyName>
-      <Welcome>
-        <img src="https://i.ibb.co/Jm1LDfF/Welcome-Portrait1.png" alt="Welcome-Portrait1" />
-        <Greet>Hi, I'm Andrey!</Greet>
-      </Welcome>
-    </div>
+      <img  src="https://i.ibb.co/Jm1LDfF/Welcome-Portrait1.png" alt="Welcome-Portrait1" />
+      <Greet>Hi, I'm Andrey!</Greet>
+      <Greet1>I build websites and mobile applications of any purpose! I am responsible for everything, starting from drafting and design, and ending with deployment and hosting! If you have a dream project, then I could help you bridge it to reality!</Greet1>
+      
+    </Welcome>
   )
 }
 
--- client/components/Pages/homeStyles.js
+++ client/components/Pages/homeStyles.js
@@ -2,27 +2,34 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { btnReset, v } from "../../styles/variables";
 
+export const Welcome = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: min-content;
+    img {
+      margin: 50px 0;
+    }
+`;
+
 export const MyName = styled.span`
     font-family: inherit;
     display: flex;
     justify-content: flex-end;
 `;
 
-export const Welcome = styled.div`
-    display: flex;
-    justify-content: center;
-    width: 100%;
-    img {
-        max-width: 80%;
-        height: auto;
-        margin: ${v.lgSpacing} ${v.xxlSpacing} ${v.lgSpacing} ${v.xxlSpacing};
-    }
-`;
-
 export const Greet = styled.span`
     font-family: inherit;
     position: relative;
-    right: 78%;
-    top: 29%;
+    left: 6%;
+    bottom: 47%;
     font-size: 25px;
+`;
+export const Greet1 = styled.span`
+    text-shadow: 1px 1px 4px #fff;
+    font-family: inherit;
+    width: 58%;
+    position: relative;
+    left: 6%;
+    bottom: 44%;
+    font-size: 17px;
 `;
