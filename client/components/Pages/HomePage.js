d567403f43ba111331e0c81f596fc3660df9ce29
Andrey Statkevskiy <andstatik@gmail.com>
2022-11-16 09:27:17 +0900

    Cosmetics: Everything up to 'Check it out!' is done, excluding theme adjustment

--- client/components/Pages/HomePage.js
+++ client/components/Pages/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import { Greet, Greet1, MyName, Welcome } from './homeStyles'
+import { Greet, Greet1, Greet2, Greet3, MyName, Welcome } from './homeStyles'
 
 /**
  * COMPONENT
@@ -13,7 +13,8 @@ export const HomePage = props => {
       <img  src="https://i.ibb.co/Jm1LDfF/Welcome-Portrait1.png" alt="Welcome-Portrait1" />
       <Greet>Hi, I'm Andrey!</Greet>
       <Greet1>I build websites and mobile applications of any purpose! I am responsible for everything, starting from drafting and design, and ending with deployment and hosting! If you have a dream project, then I could help you bridge it to reality!</Greet1>
-      
+      <Greet2>Hey there!</Greet2>
+      <Greet3>Check it out!</Greet3>
     </Welcome>
   )
 }
--- client/components/Pages/homeStyles.js
+++ client/components/Pages/homeStyles.js
@@ -20,8 +20,9 @@ export const MyName = styled.span`
 export const Greet = styled.span`
     font-family: inherit;
     position: relative;
+    width: 28%;
     left: 6%;
-    bottom: 47%;
+    bottom: 42%;
     font-size: 25px;
 `;
 export const Greet1 = styled.span`
@@ -30,6 +31,26 @@ export const Greet1 = styled.span`
     width: 58%;
     position: relative;
     left: 6%;
-    bottom: 44%;
+    bottom: 39%;
     font-size: 17px;
+`;
+export const Greet2 = styled.span`
+    text-shadow: 1px 1px 4px #fff;
+    font-family: Satisfy;
+    width: 33%;
+    position: relative;
+    left: 50%;
+    bottom: 74%;
+    font-size: 47px;
+    transform: rotate(-12deg);
+`;
+export const Greet3 = styled.span`
+    text-shadow: 1px 1px 4px #fff;
+    font-family: Satisfy;
+    width: 38%;
+    position: relative;
+    left: 34%;
+    bottom: 35%;
+    font-size: 47px;
+    transform: rotate(5deg);
 `;
