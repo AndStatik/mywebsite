f5390073c3d39ef173f2a7b18606792c81865bba
Andrey Statkevskiy <andstatik@gmail.com>
2023-01-02 19:20:51 +0900

    Enhancement: Added a 3-block div with all services

--- client/components/Pages/ServicesPage.js
+++ client/components/Pages/ServicesPage.js
@@ -1,16 +1,72 @@
 import React from "react";
 import { connect } from "react-redux";
+import {
+  MyServices,
+  Services,
+  ServicesContainer,
+  SingleService,
+  OverlayPrice
+} from "./servicesStyles";
+import {
+  Overlay,
+  OverlayTitle,
+  OverlayDesc,
+  OverlayBlur,
+  OverlayTurn,
+} from "./projectsStyles";
 
 /**
  * COMPONENT
  */
 export const ServicesPage = (props) => {
   return (
-    <div>
-      <h1>Services Page</h1>
-      <img style={{width: 350+'px'}} src="workInProgress.png" alt="Work In Progress" />
-      <p>Someday I will list my services here</p>
-    </div>
+    <ServicesContainer>
+      <MyServices>My Services</MyServices>
+      <Services>
+        <SingleService>
+          <img src="personalWeb.png" alt="Personal Website" />
+          <Overlay>
+            <OverlayTitle style={{ fontSize: 18 + "px", marginTop: 60 + "px" }}>
+              Personal Website
+            </OverlayTitle>
+            <OverlayDesc>
+              I will construct a static personal website for you!
+            </OverlayDesc>
+            <OverlayPrice>
+              {">$99"}
+            </OverlayPrice>
+          </Overlay>
+        </SingleService>
+        <SingleService>
+          <img src="mobileApp.png" alt="Mobile Application" />
+          <OverlayBlur>
+            <OverlayTitle style={{ fontSize: 18 + "px", marginTop: 60 + "px" }}>
+              Mobile Application
+            </OverlayTitle>
+            <OverlayDesc>
+              I will construct a static personal website for you!
+            </OverlayDesc>
+            <OverlayPrice>
+              {">$149"}
+            </OverlayPrice>
+          </OverlayBlur>
+        </SingleService>
+        <SingleService>
+          <img src="ecomm.png" alt="Ecommerce Website" />
+          <OverlayTurn>
+            <OverlayTitle style={{ fontSize: 18 + "px", marginTop: 60 + "px" }}>
+              E-Commerce Website
+            </OverlayTitle>
+            <OverlayDesc>
+              I will construct a static personal website for you!
+            </OverlayDesc>
+            <OverlayPrice>
+              {">$199"}
+            </OverlayPrice>
+          </OverlayTurn>
+        </SingleService>
+      </Services>
+    </ServicesContainer>
   );
 };
 
